657392b7bf89a3c88761b1b2b6f996a2387c75b6
Peng Xiao <xiaoquwl@gmail.com>
2019-07-22 10:46:58 +0200

    check docker user group before create
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

--- lab/docker/multi-node/vagrant/setup.sh
+++ lab/docker/multi-node/vagrant/setup.sh
@@ -7,9 +7,18 @@ sudo apt-get install -y git vim gcc build-essential telnet
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 
-# start docker service
-sudo groupadd docker
+if [ ! $(getent group docker) ];
+then 
+    sudo groupadd docker;
+else
+    echo "docker user group already exists"
+fi
+
 sudo gpasswd -a $USER docker
 sudo service docker restart
 
 rm -rf get-docker.sh
+
+# open password auth for backup if ssh key doesn't work, bydefault, username=vagrant password=vagrant
+# sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+# sudo service sshd restart
--- lab/docker/single-node/vagrant-centos7/setup.sh
+++ lab/docker/single-node/vagrant-centos7/setup.sh
@@ -7,9 +7,18 @@ sudo yum install -y git vim gcc glibc-static telnet
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 
-# start docker service
-sudo groupadd docker
+if [ ! $(getent group docker) ];
+then 
+    sudo groupadd docker;
+else
+    echo "docker user group already exists"
+fi
+
 sudo gpasswd -a $USER docker
 sudo systemctl start docker
 
 rm -rf get-docker.sh
+
+# open password auth for backup if ssh key doesn't work, bydefault, username=vagrant password=vagrant
+# sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+# sudo systemctl restart sshd
