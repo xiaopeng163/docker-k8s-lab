7beaf79e94b89943dc8d0419e3159a3b09bfd387
Peng Xiao <xiaoquwl@gmail.com>
2020-12-10 00:09:23 +0100

    add ubuntu20.04

--- lab/docker/single-node/vagrant-ubuntu20.04/setup.sh
+++ lab/docker/single-node/vagrant-ubuntu20.04/setup.sh
@@ -1,23 +1,24 @@
 #/bin/sh
 
 # install some tools
-sudo yum install -y git vim gcc glibc-static telnet psmisc
+sudo apt-get install -y git vim gcc build-essential telnet
 
 # install docker
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 
-if [ ! $(getent group docker) ]; then
-    sudo groupadd docker
+if [ ! $(getent group docker) ];
+then 
+    sudo groupadd docker;
 else
     echo "docker user group already exists"
 fi
 
 sudo gpasswd -a $USER docker
-sudo systemctl start docker
+sudo service docker restart
 
 rm -rf get-docker.sh
 
 # open password auth for backup if ssh key doesn't work, bydefault, username=vagrant password=vagrant
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+# sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+# sudo service sshd restart
