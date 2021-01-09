f2e8836b0d4f0d865f8d11cc760a34dfea1e8c64
Peng Xiao <xiaoquwl@gmail.com>
2021-01-09 21:10:17 +0100

    add centos8

---
+++ lab/docker/single-node/vagrant-centos8/setup.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+
+# install some tools
+sudo yum install -y git vim gcc telnet psmisc
+
+# install docker
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+if [ ! $(getent group docker) ]; then
+    sudo groupadd docker
+else
+    echo "docker user group already exists"
+fi
+
+sudo gpasswd -a $USER docker
+sudo systemctl start docker
+
+rm -rf get-docker.sh
+
+# open password auth for backup if ssh key doesn't work, bydefault, username=vagrant password=vagrant
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
